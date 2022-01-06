97d88496e07eaaa0b3e3244390993ccd055a4f1a
DavidCDCB <cristian-cb1@hotmail.com>
2022-01-06 13:20:08 -0500

    Entorno Virtual

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 #https://python-adv-web-apps.readthedocs.io/en/latest/flask3.html
 #https://stackoverflow.com/questions/20646822/how-to-serve-static-files-in-flask
 #python3 -m http.server 5500 --bind 192.168.1.104
+
+#source venv/bin/activate
 #pip3 freeze > requirements.txt
 
 #https://www.youtube.com/watch?v=-1DmVCPB6H8
 #https://dev.to/techparida/how-to-deploy-a-flask-app-on-heroku-heb
+#https://www.youtube.com/watch?v=BP3D03CYFHA
 
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
