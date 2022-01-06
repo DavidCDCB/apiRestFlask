87c94a62e7d29b1d47df2fb82b2986e5ca8ec48d
DavidCDCB <cristian-cb1@hotmail.com>
2022-01-06 11:24:04 -0500

    Deploy

---
+++ main.py
@@ -0,0 +1,34 @@
+#https://stackabuse.com/serving-static-files-with-flask
+#https://python-adv-web-apps.readthedocs.io/en/latest/flask3.html
+#https://stackoverflow.com/questions/20646822/how-to-serve-static-files-in-flask
+#python3 -m http.server 5500 --bind 192.168.1.104
+#pip3 freeze > requirements.txt
+
+#https://www.youtube.com/watch?v=-1DmVCPB6H8
+#https://dev.to/techparida/how-to-deploy-a-flask-app-on-heroku-heb
+
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# <string:user>
+@app.route('/proto/<int:user>', methods=['GET'])
+def proto(user):
+	print(user)
+	return jsonify({
+        "nombre" :"sddf"
+        })
+
+@app.route('/enviar', methods=['POST'])
+def enviar():
+	msg = request.json
+	print(msg)
+	return "Recibido"
+
+if __name__ == '__main__':
+	app.run(debug=True, host="localhost", port=7000)
+
+
+
